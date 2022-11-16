f666ddc03f05beadcf40fa2a07d46b6562a88f8e
Никита Максимов <gameboss287@gmail.com>
2022-11-16 13:20:17 +0800

    Модели поста и комментария

--- shmemes/feed/models.py
+++ shmemes/feed/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# class Post(models.Model):
-#     author_id = models.ForeignKey()
-#     text = models.CharField(max_length=255)
-#     pub_date = models.DataField('Дата публикации')
+User = get_user_model()
+
+class Post(models.Model):
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор поста')
+    text = models.CharField('Текст поста', max_length=255)
+    likes = models.IntegerField('Лайки', default=0)
+    dislikes = models.IntegerField('Дизлайки', default=0)
+    pub_date = models.DateField('Дата публикации')
+    image = models.ImageField('Изображение')
+
+class Comment(models.Model):
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост')
+    text = models.CharField('Текст', max_length=255)
--- shmemes/shmemes/settings.py
+++ shmemes/shmemes/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'user.apps.UserConfig',
+    'feed.apps.FeedConfig'
 ]
 
 MIDDLEWARE = [
