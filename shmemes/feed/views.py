5eb0e50ef53945e072d10d3f73c6901aa01f13d1
Никита Максимов <gameboss287@gmail.com>
2022-11-17 22:32:37 +0800

    Шаблон ленты

--- shmemes/feed/views.py
+++ shmemes/feed/views.py
@@ -4,4 +4,9 @@ from django.http import JsonResponse
 # Create your views here.
 
 def feed(request):
-    return render(request, 'feed.html')
+    return render(request, 'feed.html', {
+        "posts": [
+            {"title": 'First post', 'description': 'Hello world, description'},
+            {"title": 'Second post', 'description': 'Description for second post'}
+        ]
+    })
