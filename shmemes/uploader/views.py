fabd898dc6c9956ebd54fed7871f600a2e71607f
Никита Максимов <gameboss287@gmail.com>
2022-11-19 16:55:07 +0800

    загрузка мемов

--- shmemes/uploader/views.py
+++ shmemes/uploader/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
-# Create your views here.
+def index(request): 
+    form = PostForm()
+    return render(request, 'index.html', {'form': form})
+
+def publish(request):
+    form = PostForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('/feed')
