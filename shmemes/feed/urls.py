2c0a89915756284cfd9a4c0516c2b81320a6b082
Nikita <n.maksimov@ispsystem.com>
2022-11-22 15:46:35 +0800

    Поправил api лайков

--- shmemes/feed/urls.py
+++ shmemes/feed/urls.py
@@ -5,8 +5,8 @@ from . import views, controller
 urlpatterns = [
     path('', views.feed, name='feed'),
     
-    path('addcomment/', controller.add_comment),
-    path('getcomments/', controller.get_comments),
+    path('api/addcomment/', controller.add_comment),
+    path('api/getcomments/', controller.get_comments),
 
     path('api/like/', controller.like),
     path('api/dislike/', controller.dislike),
